c97328ca5628ff0698586531eabdfd7e644eaae6
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-19 00:41:56 +0500

    DbContext registration error fixed

--- src/BookManagement.API/Program.cs
+++ src/BookManagement.API/Program.cs
@@ -8,10 +8,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
