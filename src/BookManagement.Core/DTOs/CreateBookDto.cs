c4c614b74306d89663581bb95e3825a80bda04ac
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-19 02:07:23 +0500

    CreateBookDto

---
+++ src/BookManagement.Core/DTOs/CreateBookDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BookManagement.Data.Entities;
+
+namespace BookManagement.Core.DTOs;
+
+public class CreateBookDto
+{
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required, Range(1, 2025)]
+    public int PublicationYear { get; set; }
+
+    [Required]
+    public string AuthorName { get; set; } = string.Empty;
+
+    public Book ToEntity()
+    {
+        return new Book
+        {
+            Title = Title,
+            PublicationYear = PublicationYear,
+            AuthorName = AuthorName
+        };
+    }
+}
