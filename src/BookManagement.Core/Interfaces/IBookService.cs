757dd318f1a546551c714ad07e84add212a71709
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-19 02:20:12 +0500

    Add IBookService interface and implement AddAsync method

---
+++ src/BookManagement.Core/Interfaces/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Core.DTOs;
+using BookManagement.Data.Entities;
+
+namespace BookManagement.Core.Interfaces;
+
+public interface IBookService
+{
+    Task<int> AddAsync(CreateBookDto dto);
+}
---
+++ src/BookManagement.Core/Services/BookService.cs
@@ -0,0 +1,21 @@
+﻿using BookManagement.Core.DTOs;
+using BookManagement.Core.Exceptions;
+using BookManagement.Core.Interfaces;
+using BookManagement.Data.Repositories;
+
+namespace BookManagement.Core.Services;
+
+public class BookService(IBookRepository repository) : IBookService
+{
+    public async Task<int> AddAsync(CreateBookDto dto)
+    {
+        if (await repository.AnyAsync(book => !book.IsDeleted && book.Title == dto.Title))
+        {
+            throw new TitleAlreadyExistsException(dto.Title);
+        }
+
+        var book = CreateBookDto.ToEntity(dto);
+        await repository.AddAsync(book);
+        return book.Id;
+    }
+}
