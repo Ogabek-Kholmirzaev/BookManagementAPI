3ef4217402e2f71baa629253e628ece4f1112504
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-19 00:16:34 +0500

    Book entity

---
+++ src/BookManagement.Data/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookManagement.Data.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public int PublicationYear { get; set; }
+    public required string AuthorName { get; set; }
+    public int ViewsCount { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
