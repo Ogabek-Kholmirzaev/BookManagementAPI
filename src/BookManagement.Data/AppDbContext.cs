0e8ccdc6d0bb38f97838527b28bbf2acacd8bcab
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-19 00:28:47 +0500

    DbContext

---
+++ src/BookManagement.Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookManagement.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>()
+            .HasIndex(book => book.Title)
+            .IsUnique();
+    }
+}
